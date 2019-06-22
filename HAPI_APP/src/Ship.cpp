fd4c60a6c77535273dfeb74f8d9db39a75d037e4
Ryan Swann <RyanSwann1992@gmail.com>
2019-06-22 12:04:32 +0100

    Ship Selection Logic Change

--- HAPI_APP/src/Ship.cpp
+++ HAPI_APP/src/Ship.cpp
@@ -136,6 +136,11 @@ void Ship::disableAction()
 
 void Ship::startMovement(Map& map)
 {
+	if (m_movementArea.m_tileArea.empty())
+	{
+		return;
+	}
+
 	if (!m_destinationSet)
 	{
 		m_destinationSet = true;
