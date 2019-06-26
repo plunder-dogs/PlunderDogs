45913b6dc45e2db5848500a90bc7760c686e717f
Ryan Swann <RyanSwann1992@gmail.com>
2019-06-26 16:32:36 +0100

    Commented out due to compiler error.

--- HAPI_APP/src/CircularBuffer.h
+++ HAPI_APP/src/CircularBuffer.h
@@ -1,35 +1,40 @@
-#pragma once
 
-#include <mutex>
-#include <queue>
 
-struct Message
-{
-	//Put something in here
-};
+//TODO: Getting compilation error
 
-#define NO_MESSAGE Message()
 
-class CircularBuffer
-{
-public:
-	CircularBuffer(int capacity);
-
-	~CircularBuffer() { delete[] buffer; }
-
-	void deposit(Message data);
-
-	Message fetch();
-
-private:
-	Message* buffer;
-	const int bufferSize;
-
-	int front;
-	int rear;
-	int count;
-
-	std::mutex lock;
-
-	std::queue<Message> overspill;
-};
+//#pragma once
+//
+//#include <mutex>
+//#include <queue>
+//
+//struct Message
+//{
+//	//Put something in here
+//};
+//
+//#define NO_MESSAGE Message()
+//
+//class CircularBuffer
+//{
+//public:
+//	CircularBuffer(int capacity);
+//
+//	~CircularBuffer() { delete[] buffer; }
+//
+//	void deposit(Message data);
+//
+//	Message fetch();
+//
+//private:
+//	Message* buffer;
+//	const int bufferSize;
+//
+//	int front;
+//	int rear;
+//	int count;
+//
+//	std::mutex lock;
+//
+//	std::queue<Message> overspill;
+//};
